980721b8cfb22f69a8f1dc7a9456fe07ae922cbc
manuel14mds <manuel14mds@gmail.com>
2022-12-07 17:40:03 -0500

    challenge finish

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@
 ## LINK DE PRUEBA POSTMAN
 
 https://api.postman.com/collections/21925648-dbfc8602-7e6b-4db4-8c5a-4d4d2f15b255?access_key=PMAT-01GKPYGJ2F6FVK9E1Q1ASHPCJ4
+
+## swager rute : http://localhost:8080/api/docs/
--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 import express from 'express'
 import userRouter from './routes/user.router.js'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUiExpress from 'swagger-ui-express'
+
+import __dirname from './utils.js'
 
 const app = express()
+const swaggerOptions = {
+    definition:{
+        openapi:'3.0.1',
+        info:{
+            title:"Api swagger",
+            description:"Desafio swagger"
+        }
+    },
+    apis:[`${__dirname}/docs/**/*.yaml`]
+}
+const specs = swaggerJsdoc(swaggerOptions)
 
 app.listen(8080, ()=>console.log('running on 8080 port'))
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send('HOLA MUNDO')
+app.get('/', (req,res)=>{
+    res.send('Route to see swaggerUi: /api/docs')
 })
+app.use('/api/docs',swaggerUiExpress.serve,swaggerUiExpress.setup(specs))
 app.use('/api/users', userRouter)
